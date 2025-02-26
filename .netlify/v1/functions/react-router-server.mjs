fc0bb6acdecca1bdca1d461c91ad285921aa3ec2
Leanitech <akarmakar846@gmail.com>
2025-02-26 18:39:04 +0530

    Update .gitignore to include .netlify and add react-router server handler function

---
+++ .netlify/v1/functions/react-router-server.mjs
@@ -0,0 +1,10 @@
+
+    export { default } from "../../../build/server/nodejs-eyJydW50aW1lIjoibm9kZWpzIn0/server.js";
+
+    export const config = {
+      name: "React Router server handler",
+      generator: "@netlify/vite-plugin-react-router@1.0.0",
+      path: "/*",
+      preferStatic: true,
+    };
+    
